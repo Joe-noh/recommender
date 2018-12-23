59670840edfbd4531bb56b4880a9751b7b3a1708
Joe-noh <goflb.jh@gmail.com>
2018-12-23 22:40:14 +0900

    returns predicted results in array

--- material/models.py
+++ material/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from surprise import Dataset, Reader, NMF
+from os import path
 
-# Create your models here.
+# plain python class
+class UserBasedMaterialRecommender:
+    alg = NMF()
+    data = None
+    trainset = None
+
+    @classmethod
+    def fit(klass):
+        data_path = path.join(settings.BASE_DIR, 'dataset.csv')
+        reader = Reader(line_format='user item rating', sep=',', rating_scale=(0, 2))
+
+        klass.data = Dataset.load_from_file(data_path, reader)
+        klass.trainset = klass.data.build_full_trainset()
+        klass.alg.fit(klass.trainset)
+
+    @classmethod
+    def predict(klass, user_id):
+        proc = lambda iid: klass.alg.predict(str(user_id), klass.trainset.to_raw_iid(iid))
+        return list(map(proc, klass.trainset.all_items()))
